3e8f0f223b22ad0ab8e1263b81618dedb06585ec
ArjunJaiswal1 <1hk17me010@hkbk.edu.in>
2022-02-11 00:40:31 +0530

    [Arjun] Add ; Added Executed

---
+++ UserRegisterationProgram/Pattern.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace RegexPattern
+{
+    public class Pattern
+    {
+        public static string REGEX_FIRSTNAME = "^[A-Z]{1}[A-Za-z]{2,}$"; //Arjun
+
+        //Method for first name 
+        public bool validateFirstName(string fname)
+        {
+
+            return Regex.IsMatch(fname, REGEX_FIRSTNAME);
+        }
+    }
+}
---
+++ UserRegisterationProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace RegexPattern
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Pattern pattern = new Pattern();
+            Console.WriteLine(pattern.validateFirstName("Arjun"));
+        }
+    }
+}
